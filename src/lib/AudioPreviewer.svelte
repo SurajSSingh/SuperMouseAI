c2741176767938195c726d5c5d21dbe4e7d8221a
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-02-12 17:21:32 -0800

    feat: :sparkles: Basic Audio previewer component

---
+++ src/lib/AudioPreviewer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { type IMediaRecorder } from "extendable-media-recorder";
+    interface AudioPreviewerProps {
+        currentURL?: string;
+    }
+    // Props
+    let { currentURL }: AudioPreviewerProps = $props();
+</script>
+
+<h2 class="text-lg text-center">Audio Preview</h2>
+<p class="text-xs text-center">{currentURL ?? "No URL"}</p>
+<audio class="w-full" controls src={currentURL}></audio>
