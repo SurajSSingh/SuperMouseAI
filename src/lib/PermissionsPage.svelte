2ea1befa14c1bb184f72c19d6a7490d1f4d55cf8
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-02-18 17:02:26 -0800

    feat: :sparkles: Update permission page to show granted permissions

--- src/lib/PermissionsPage.svelte
+++ src/lib/PermissionsPage.svelte
@@ -3,6 +3,7 @@
     import { type RecordingStates } from "$lib/types";
     import MicRecorder from "$lib/MicRecorder.svelte";
     import { NotificationSystem } from "./notificationSystem.svelte";
+    import Status from "./components/Status.svelte";
 
     interface PermissionsPageProps {
         setupRecorder: () => Promise<void>;
@@ -18,6 +19,18 @@
         testNotify = false,
     }: PermissionsPageProps = $props();
 
+    let microphonePermission = $state(false);
+
+    $effect(() => {
+        const queryPermissions = async () => {
+            microphonePermission =
+                // @ts-ignore 'microphone' should be querable for permissions
+                (await navigator.permissions.query({ name: "microphone" }))
+                    .state === "granted";
+        };
+        queryPermissions();
+    });
+
     async function resetPermission() {
         // await invoke("reset_permission", { origin: window.origin });
         await setupRecorder();
@@ -26,24 +39,50 @@
     }
 </script>
 
+{#snippet PermissionRow(
+    name: string,
+    status: boolean,
+    disabled: boolean,
+    onclick: () => void,
+    icon?: string,
+)}
+    <div>
+        <h3>
+            <Status
+                color={status ? "success" : "error"}
+                size="lg"
+                class="mr-2"
+            />{icon ? `${icon}: ` : ""}{name}
+        </h3>
+        <span class="pl-0 text-xs"
+            >Permission: {status ? "Granted" : "Denied"}</span
+        >
+    </div>
+    <Button
+        color="secondary"
+        size="sm"
+        class="m-2 text-xs"
+        {disabled}
+        {onclick}
+    >
+        Ask {icon || name} Permission Again
+    </Button>
+{/snippet}
+
 <h2 class="text-md text-center">Check Permissions</h2>
 <div class="grid grid-cols-2 my-1">
-    <Button
-        color="secondary"
-        size="sm"
-        class="m-2"
-        disabled={recordingState !== "stopped"}
-        onclick={resetPermission}
-    >
-        Ask Microphone (🎤) Permission Again
-    </Button>
-    <Button
-        color="secondary"
-        size="sm"
-        class="m-2"
-        onclick={() => notifier.getPermissionToNotify(testNotify)}
-        disabled={notifier.permissionGranted}
-    >
-        Ask Notification (🔔) Permission Again
-    </Button>
+    {@render PermissionRow(
+        "Microphone",
+        microphonePermission,
+        recordingState !== "stopped",
+        resetPermission,
+        "🎤",
+    )}
+    {@render PermissionRow(
+        "Notification",
+        notifier.permissionGranted,
+        notifier.permissionGranted,
+        () => notifier.getPermissionToNotify(testNotify),
+        "🔔",
+    )}
 </div>
