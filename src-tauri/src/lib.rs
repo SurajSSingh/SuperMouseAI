a65a8ea74233c7e09779737c9d1a978eac9b13b6
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-02-26 11:51:34 -0800

    feat: :sparkles: Enforce Single Instance
    App will only allow one instance as a way to prevent the user from creating multiple windows, which may lead to an error with multiple recorders

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -165,6 +165,12 @@ struct ModKeyEvent {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_notification::init())
         .setup(|app| {
