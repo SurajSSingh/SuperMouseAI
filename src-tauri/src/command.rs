aa4cc78810d13bb3735b233a1809c8d897b418aa
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-03-11 20:46:04 -0700

    refactor(backend): :pencil2: Fix casing for override param in set window top command

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -146,8 +146,8 @@ pub async fn paste_text(text: String) -> Result<(), String>{
 #[tauri::command]
 #[specta::specta]
 /// Put window on top
-pub async fn set_window_top(webview_window: tauri::WebviewWindow, overrideValue: Option<bool>) -> Result<(), String> {
-  Ok(webview_window.set_always_on_top(overrideValue.unwrap_or(true)).map_err(|err| {
+pub async fn set_window_top(webview_window: tauri::WebviewWindow, override_value: Option<bool>) -> Result<(), String> {
+  Ok(webview_window.set_always_on_top(override_value.unwrap_or(true)).map_err(|err| {
     log::error!("Could not set window to top: {}", err);
     err.to_string()
   })?)
