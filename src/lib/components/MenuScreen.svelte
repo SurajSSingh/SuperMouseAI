d899a25ba035f3aaf1dd8b9a15510d9cb7df6061
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-03-25 16:22:03 -0700

    refactor(frontend): :recycle: Move Menu items directly into MenuScreen
    Move tabs for options into menu screen component, also fixes the bug with light mode tabs
    
    resolves #30

--- src/lib/components/MenuScreen.svelte
+++ src/lib/components/MenuScreen.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
     import * as Dialog from "$lib/components/ui/dialog/index.js";
     import type { Snippet } from "svelte";
+    import Tab from "./ui/Tab.svelte";
+    import WhisperOptions from "./WhisperOptions.svelte";
+    import AppOptions from "./AppOptions.svelte";
+    import DangerZone from "./DangerZone.svelte";
 
     interface MenuScreenProps {
         open?: boolean;
         title?: string;
         description?: string;
         class?: string;
-        children?: Snippet;
     }
 
     let {
@@ -15,7 +18,6 @@
         title = "Menu",
         description = "Menu for the application",
         class: className = "",
-        children,
     }: MenuScreenProps = $props();
 </script>
 
@@ -25,13 +27,45 @@
     >
     <Dialog.Content class={`${className}`}>
         <div>
-            <Dialog.Title><h2 class="text-primary">{title}</h2></Dialog.Title>
+            <Dialog.Title><h2 class="">{title}</h2></Dialog.Title>
             <Dialog.Description>
-                <span class="text-primary">{description}</span>
+                <span class="">{description}</span>
             </Dialog.Description>
         </div>
         <div class=" overflow-auto">
-            {@render children?.()}
+            <section class="tabs tabs-lift w-full">
+                <Tab
+                    value="tabs"
+                    label="Configuration"
+                    class="bg-base-100 border-base-300 p-6"
+                    inputClass="not-checked:bg-base-200 not-checked:text-accent-content hover:font-bold hover:text-base-content border-base-300"
+                    checked
+                >
+                    <div class="h-60 overflow-auto pr-6">
+                        <WhisperOptions />
+                    </div>
+                </Tab>
+                <Tab
+                    value="tabs"
+                    label="App Options"
+                    class="bg-base-100 border-base-300 p-6"
+                    inputClass="not-checked:bg-base-200 not-checked:text-accent-content hover:font-bold hover:text-base-content border-base-300"
+                >
+                    <div class="h-60 overflow-auto pr-6">
+                        <AppOptions />
+                    </div>
+                </Tab>
+                <Tab
+                    value="tabs"
+                    label="Danger Zone"
+                    inputClass="input-ghost p-6 hover:bg-error hover:font-bold not-checked:bg-base-200 not-checked:text-warning-content checked:input-xl checked:text-warning"
+                    class="bg-base-100 border-base-300 p-6"
+                >
+                    <div class="h-60 overflow-auto pr-6">
+                        <DangerZone bind:isDialogOpen={open} />
+                    </div>
+                </Tab>
+            </section>
         </div>
     </Dialog.Content>
 </Dialog.Root>
