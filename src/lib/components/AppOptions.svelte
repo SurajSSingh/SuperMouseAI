f044ac24d7de96d25158636702796e825e2fe409
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-04-01 19:49:10 -0700

    refactor(frontend): :recycle: Unify App configuration
    Move app option toggles into single collapsable fieldset

--- src/lib/components/AppOptions.svelte
+++ src/lib/components/AppOptions.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { commands } from "$lib/bindings";
     import { configStore } from "$lib/store.svelte";
+    import CollapseableFieldSet from "./ui/CollapseableFieldSet.svelte";
     import ToggleSwitch from "./ui/ToggleSwitch.svelte";
 
     interface AppOptionProps {}
@@ -9,45 +10,51 @@
 </script>
 
 <h2 class="text-md text-center">Application options.</h2>
-<fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
-    <ToggleSwitch
-        label="Window Always on Top:"
-        bind:checked={configStore.windowFloat.value}
-        onchange={(e) => commands.setWindowTop(e.currentTarget.checked)}
-    />
-    <p class="fieldset-label">
-        This will make it so the window is always on top.
-    </p>
-</fieldset>
-<fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
-    <ToggleSwitch
-        label="Auto-Paste:"
-        bind:checked={configStore.autoPaste.value}
-    />
-    <p class="fieldset-label">
-        Immediately paste the value after transcribing (in active area selected
-        by cursor).
-    </p>
-</fieldset>
-<fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
-    <ToggleSwitch
-        label="Use Ctrl+V for auto-paste:"
-        bind:checked={configStore.pasteViaKeys.value}
-    />
-    <p class="text-base-content/60">
-        When toggled on, auto-paste with <kbd class="kbd kbd-sm text-accent"
-            >Ctrl + V</kbd
-        >. When off, it will write the text (NOTE: uses
-        <kbd class="kbd kbd-sm text-accent">Enter</kbd> for newlines)
-    </p>
-</fieldset>
-<fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
-    <ToggleSwitch
-        label="Remove Inter-space Newlines:"
-        bind:checked={configStore.interNLRemove.value}
-    />
-    <p class="fieldset-label">
-        Will remove extra new-lines that happen inside a sentence (between words
-        without puncuation).
-    </p>
-</fieldset>
+<CollapseableFieldSet
+    title="General Configuration"
+    titleTag="h3"
+    subtitle="Configure how the app operates"
+>
+    <div class="mb-4">
+        <ToggleSwitch
+            label="Window Always on Top:"
+            bind:checked={configStore.windowFloat.value}
+            onchange={(e) => commands.setWindowTop(e.currentTarget.checked)}
+        />
+        <p class="fieldset-label">
+            This will make it so the window is always on top.
+        </p>
+    </div>
+    <div class="mb-4">
+        <ToggleSwitch
+            label="Auto-Paste:"
+            bind:checked={configStore.autoPaste.value}
+        />
+        <p class="fieldset-label">
+            Immediately paste the value after transcribing (in active area
+            selected by cursor).
+        </p>
+    </div>
+    <div class="mb-4">
+        <ToggleSwitch
+            label="Use Ctrl+V for auto-paste:"
+            bind:checked={configStore.pasteViaKeys.value}
+        />
+        <p class="text-base-content/60">
+            When toggled on, auto-paste with <kbd class="kbd kbd-sm text-accent"
+                >Ctrl + V</kbd
+            >. When off, it will write the text (NOTE: uses
+            <kbd class="kbd kbd-sm text-accent">Enter</kbd> for newlines)
+        </p>
+    </div>
+    <div class="mb-4">
+        <ToggleSwitch
+            label="Remove Inter-space Newlines:"
+            bind:checked={configStore.interNLRemove.value}
+        />
+        <p class="fieldset-label">
+            Will remove extra new-lines that happen inside a sentence (between
+            words without puncuation).
+        </p>
+    </div>
+</CollapseableFieldSet>
