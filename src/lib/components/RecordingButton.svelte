c830cdcc2eff6afdac8d2350889ede6dec469d71
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-05-12 18:06:49 -0700

    feat(ui): :sparkles: New recording and transcription viewer components
    Modify recording button to work with the new recording system, create new transcription viewer whose only job is to show transcripts without processing them

--- src/lib/components/RecordingButton.svelte
+++ src/lib/components/RecordingButton.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
     import { commands } from "$lib/bindings";
     import { notifier } from "$lib/notificationSystem.svelte";
+    import { debug } from "@tauri-apps/plugin-log";
     import { Button } from "./ui/button";
-    // import Loading from "./ui/Loading.svelte";
+    import Loading from "./ui/Loading.svelte";
 
     interface RecorderProps {
         id?: string;
         // recordingState: RecordingStates;
         onRecordingStart?: () => void;
-        onRecordingEnd?: (chunks: Blob[]) => void;
+        onRecordingEnd?: () => Promise<void>;
         onError?: (err: string) => void;
         disabled?: boolean;
     }
 
     let {
-        id = "audio-holder",
+        id = "audio-recorder",
         // recordingState = "stopped",
         onRecordingStart,
         onRecordingEnd,
@@ -24,31 +25,53 @@
 
     let isRecording = $state(false);
     let isProcessing = $state(false);
-    async function toggleRecording() {
+
+    const buttonText = $derived(
+        isRecording
+            ? "Stop Recording"
+            : isProcessing
+              ? "Processing..."
+              : "Start Recording",
+    );
+    const recordingText = $derived(
+        isRecording
+            ? "(🎤🔴): Microphone RECORDING"
+            : isProcessing
+              ? "(🗣️📝): Processing Audio"
+              : "(🎤⏹️): Microphone Inactive",
+    );
+    export async function toggleRecording() {
         if (isProcessing) return;
         if (isRecording) {
+            debug("Switch to processing mode.");
             isRecording = false;
             isProcessing = true;
-            const res = await commands.stopTranscribeAndProcessData(
-                null,
-                "Skip",
-                {
-                    normalize_result: true,
-                    denoise_audio: null,
-                    high_pass_value: null,
-                    low_pass_value: null,
-                },
-            );
-            if (res.status === "ok") {
-                isProcessing = false;
-                notifier.showToast(res.data, "success");
-            } else {
-                notifier.showToast(res.error, "error");
+            const stopped = await commands.stopMicrophoneRecording(1000);
+            if (stopped.status === "error") {
+                onError?.(`Stopping microphone failed: ${stopped.error}`);
+                return;
             }
+            await onRecordingEnd?.();
+            // const res = await commands.transcribeCurrentData(null, {
+            //     normalize_result: true,
+            //     denoise_audio: null,
+            //     high_pass_value: null,
+            //     low_pass_value: null,
+            // });
+            // if (res.status === "ok") {
+            //     notifier.showToast(res.data, "success");
+            // } else {
+            //     onError?.(`Processing failed: ${res.error}`);
+            // }
+            isProcessing = false;
         } else {
             const res = await commands.startMicrophoneRecording();
             if (res.status === "ok") {
-                isRecording = true;
+                debug(
+                    "Use result of audio recorder creation to signify recording",
+                );
+                isRecording = res.data;
+                if (isRecording) onRecordingStart?.();
             } else {
                 notifier.showToast(res.error, "error");
             }
@@ -69,6 +92,12 @@
         size="sm"
         width="block"
         onclick={toggleRecording}
-        disabled={disabled || isProcessing}>Test Recording</Button
+        disabled={disabled || isProcessing}>{buttonText}</Button
     >
+    <p class="mb-2">
+        {#if isProcessing}
+            <Loading />
+        {/if}
+        {recordingText}
+    </p>
 </section>
